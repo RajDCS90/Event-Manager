1933ea16b025038aaabde932f9c7d2fbfcf93019
rajzzz10 <raj90.dcs@gmail.com>
2025-04-11 15:19:12 +0530

    static app done next we will integrate the backend in it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { AppProvider } from './context/AppProvider';
 import Layout from './components/common/Layout';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <AppProvider>
         <Layout>
           <Routes>
+            <Route path="/login" element={<Login />} />
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
